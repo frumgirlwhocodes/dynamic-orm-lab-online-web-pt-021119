f53d5b7aea8f26caab846c5760955c65fd0ad683
lieby weintraub <liebahinda@gmail.com>
2019-05-05 05:56:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,5 +36,5 @@ end
         values.join(", ")
   end 
   def save 
-    sql= "INSERT INTO #{table_name_for_insert} (#{" 
+    sql= "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})" 
 end 
