5d6be7665cc527452e8ec87f2e7622bfd04df53c
lieby weintraub <liebahinda@gmail.com>
2019-05-05 05:21:36 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,7 +5,7 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
-def self.colume_names
+def self.column_names
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
